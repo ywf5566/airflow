910af32768cd1de17d804f0fd06c61048fe189f3
wenfeng.yu <ywf690912537>
2020-12-29 14:26:24 +0800

    kd03 同步分钟线任务添加数据治理任务

--- dags/kd03-dags/sync_minquota.py
+++ dags/kd03-dags/sync_minquota.py
@@ -12,6 +12,13 @@ dag = DAG('sync_minquota',
           catchup=False,
           start_date=datetime(2020, 12, 17, 17, 0))
 
-sync_minquota = BashOperator(task_id="sync_minquota", bash_command="sh /usr/lib/carter/dbsync/scripts/sync_minquota.sh ", dag=dag)
-update_qsdata = BashOperator(task_id="update_qsdata", bash_command="sh /usr/lib/carter/dbsync/scripts/update_qsdata.sh ", dag=dag)
-sync_minquota >> update_qsdata
+sync_minquota = BashOperator(task_id="sync_minquota",
+                             bash_command="sh /usr/lib/carter/dbsync/scripts/sync_minquota.sh ", dag=dag)
+update_qsdata = BashOperator(task_id="update_qsdata",
+                             bash_command="sh /usr/lib/carter/dbsync/scripts/update_qsdata.sh ", dag=dag)
+statistic_daily_basic_data = BashOperator(task_id="statistic_daily_basic_data",
+                                          bash_command="sh /usr/lib/carter/dbsync/scripts/statistic_daily_basic_data"
+                                                       ".sh ",
+                                          dag=dag)
+
+sync_minquota >> update_qsdata >> statistic_daily_basic_data

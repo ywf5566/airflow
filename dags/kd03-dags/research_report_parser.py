d5f6c93cc7df3fb9e016c187e3c21fd5852f7e6c
wenfeng.yu <ywf690912537>
2021-01-18 11:17:15 +0800

    kd03 添加任务

---
+++ dags/kd03-dags/research_report_parser.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+
+
+default_args = {
+    'owner': 'kd01_update'
+}
+dag = DAG(
+    'research_report_parser',
+    default_args=default_args,
+    description='beta_info_update',
+    schedule_interval='0 20 * * *',
+    catchup=False,
+    start_date=datetime(2021, 1, 16, 20, 0)
+)
+
+parse_research_report = BashOperator(task_id="parse_research_report", bash_command="cd /usr/lib/carter/event-news-scheduler;sh project/extractor/script/parse_research_report.sh prod ", dag=dag)
+research_report_opinion_detection = BashOperator(task_id="research_report_opinion_detection", bash_command="cd /usr/lib/carter/event-news-scheduler;sh project/extractor/script/opinion_detection.sh prod ", dag=dag)
+parse_research_report >> research_report_opinion_detection

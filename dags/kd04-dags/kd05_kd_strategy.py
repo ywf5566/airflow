8301e1a5552eb833fc40964687f947d71795ce45
wenfeng.yu <ywf690912537>
2021-01-22 11:24:47 +0800

    kd03 kd04 任务更新

--- dags/kd04-dags/kd05_kd_strategy.py
+++ dags/kd04-dags/kd05_kd_strategy.py
@@ -11,37 +11,23 @@ dag = DAG('kd05_kd_strategy',
           catchup=False,
           start_date=datetime(2020, 12, 24, 16, 0))
 
-job_start_task = SSHOperator(task_id="job_start_task", ssh_conn_id="kd05_keydriver",
-                             command="sh /usr/lib/carter/kd_strategy/script/monitor_start_task.sh prod ", dag=dag)
-v3_dk_daily_task = SSHOperator(task_id="v3_dk_daily_task", ssh_conn_id="kd05_keydriver",
-                               command="sh /usr/lib/carter/kd_strategy/script/v3_dk_daily_task.sh prod ", dag=dag)
-block_indicator_task = SSHOperator(task_id="block_indicator_task", ssh_conn_id="kd05_keydriver",
-                                   command="sh /usr/lib/carter/kd_strategy/script/v4_block_indicator_task.sh prod ",
-                                   dag=dag)
-indicator_daily_task = SSHOperator(task_id="indicator_daily_task", ssh_conn_id="kd05_keydriver",
-                                   command="sh /usr/lib/carter/kd_strategy/script/indicator_daily_task.sh prod ",
-                                   dag=dag)
-v3_model_rsync = SSHOperator(task_id="v3_model_rsync", ssh_conn_id="kd05_keydriver",
-                             command="sh /usr/lib/carter/kd_strategy/script/v3_model_rsync.sh kd01 ", dag=dag)
-stock_indicator_task = SSHOperator(task_id="stock_indicator_task", ssh_conn_id="kd05_keydriver",
-                                   command="sh /usr/lib/carter/kd_strategy/script/v4_stock_indicator_task.sh prod ",
-                                   dag=dag)
-v3_src_strategy_daily_task = SSHOperator(task_id="v3_src_strategy_daily_task", ssh_conn_id="kd05_keydriver",
-                                         command="sh /usr/lib/carter/kd_strategy/script/v3_src_strategy_daily_task.sh prod ",
-                                         dag=dag)
-strategy_report_week_task = SSHOperator(task_id="strategy_report_week_task", ssh_conn_id="kd05_keydriver",
-                                        command="sh /usr/lib/carter/kd_strategy/script/strategy_report_week_task.sh prod ",
-                                        dag=dag)
-v3_strategy_daily_task = SSHOperator(task_id="v3_strategy_daily_task", ssh_conn_id="kd05_keydriver",
-                                     command="sh /usr/lib/carter/kd_strategy/script/v3_strategy_daily_task.sh prod ",
-                                     dag=dag)
-job_end_task = SSHOperator(task_id="job_end_task", ssh_conn_id="kd05_keydriver",
-                           command="sh /usr/lib/carter/kd_strategy/script/monitor_end_task.sh prod ", dag=dag)
+job_start_task = SSHOperator(task_id="job_start_task", ssh_conn_id="kd01_keydriver",command="sh /usr/lib/carter/kd_strategy/script/monitor_start_task.sh prod ", dag=dag)
+v3_dk_daily_task = SSHOperator(task_id="v3_dk_daily_task", ssh_conn_id="kd01_keydriver",command="sh /usr/lib/carter/kd_strategy/script/v3_dk_daily_task.sh prod ", dag=dag)
+block_indicator_task = SSHOperator(task_id="block_indicator_task", ssh_conn_id="kd01_keydriver",command="sh /usr/lib/carter/kd_strategy/script/v4_block_indicator_task.sh prod ", dag=dag)
+stockrnn_daily_task = SSHOperator(task_id="stockrnn_daily_task", ssh_conn_id="kd01_keydriver",command="sh /usr/lib/carter/kd_strategy/script/stockrnn_daily_task.sh prod ", dag=dag)
+indicator_daily_task = SSHOperator(task_id="indicator_daily_task", ssh_conn_id="kd01_keydriver",command="sh /usr/lib/carter/kd_strategy/script/indicator_daily_task.sh prod ", dag=dag)
+v3_model_rsync = SSHOperator(task_id="v3_model_rsync", ssh_conn_id="kd01_keydriver",command="sh /usr/lib/carter/kd_strategy/script/v3_model_rsync.sh kd06 ", dag=dag)
+stock_indicator_task = SSHOperator(task_id="stock_indicator_task", ssh_conn_id="kd01_keydriver",command="sh /usr/lib/carter/kd_strategy/script/v4_stock_indicator_task.sh prod ", dag=dag)
+v3_src_strategy_daily_task = SSHOperator(task_id="v3_src_strategy_daily_task", ssh_conn_id="kd01_keydriver",command="sh /usr/lib/carter/kd_strategy/script/v3_src_strategy_daily_task.sh prod ", dag=dag)
+strategy_report_week_task = SSHOperator(task_id="strategy_report_week_task", ssh_conn_id="kd01_keydriver",command="sh /usr/lib/carter/kd_strategy/script/strategy_report_week_task.sh prod ", dag=dag)
+v3_strategy_daily_task = SSHOperator(task_id="v3_strategy_daily_task", ssh_conn_id="kd01_keydriver",command="sh /usr/lib/carter/kd_strategy/script/v3_strategy_daily_task.sh prod ", dag=dag)
+job_end_task = SSHOperator(task_id="job_end_task", ssh_conn_id="kd01_keydriver",command="sh /usr/lib/carter/kd_strategy/script/monitor_end_task.sh prod ", dag=dag)
 
 stock_indicator_task >> [strategy_report_week_task]
 block_indicator_task >> [stock_indicator_task]
-job_start_task >> [v3_dk_daily_task, block_indicator_task, indicator_daily_task, v3_model_rsync]
+job_start_task >> [v3_dk_daily_task, block_indicator_task, stockrnn_daily_task, indicator_daily_task, v3_model_rsync]
 strategy_report_week_task >> [job_end_task]
+stockrnn_daily_task >> [job_end_task]
 indicator_daily_task >> [job_end_task]
 v3_dk_daily_task >> [job_end_task]
 v3_strategy_daily_task >> [job_end_task]
--- dags/kd04-dags/kd05_kdalpha_daily_am_task.py
+++ dags/kd04-dags/kd05_kdalpha_daily_am_task.py
@@ -11,17 +11,10 @@ dag = DAG('kd05_kdalpha_daily_am_task',
           catchup=False,
           start_date=datetime(2020, 12, 24, 16, 0))
 
-kdalpha_am_start_task = SSHOperator(task_id="kdalpha_am_start_task", ssh_conn_id="kd05_keydriver",
-                                    command="sh /usr/lib/carter/kd_strategy/script/kdalpha_am_start_task.sh prod ",
-                                    dag=dag)
-kdalpha_daily_am_task = SSHOperator(task_id="kdalpha_daily_am_task", ssh_conn_id="kd05_keydriver",
-                                    command="sh /usr/lib/carter/kd_strategy/script/kdalpha_strategy_daily_am_task.sh prod ",
-                                    dag=dag)
-kdalpha_strategy_rank_task = SSHOperator(task_id="kdalpha_strategy_rank_task", ssh_conn_id="kd05_keydriver",
-                                         command="sh /usr/lib/carter/kd_strategy/script/kdalpha_strategy_rank_task.sh prod ",
-                                         dag=dag)
-kdalpha_am_end_task = SSHOperator(task_id="kdalpha_am_end_task", ssh_conn_id="kd05_keydriver",
-                                  command="sh /usr/lib/carter/kd_strategy/script/kdalpha_am_end_task.sh prod ", dag=dag)
+kdalpha_am_start_task = SSHOperator(task_id="kdalpha_am_start_task", ssh_conn_id="kd01_keydriver",command="sh /usr/lib/carter/kd_strategy/script/kdalpha_am_start_task.sh prod ", dag=dag)
+kdalpha_daily_am_task = SSHOperator(task_id="kdalpha_daily_am_task", ssh_conn_id="kd01_keydriver",command="sh /usr/lib/carter/kd_strategy/script/kdalpha_strategy_daily_am_task.sh prod ", dag=dag)
+kdalpha_strategy_rank_task = SSHOperator(task_id="kdalpha_strategy_rank_task", ssh_conn_id="kd01_keydriver",command="sh /usr/lib/carter/kd_strategy/script/kdalpha_strategy_rank_task.sh prod ", dag=dag)
+kdalpha_am_end_task = SSHOperator(task_id="kdalpha_am_end_task", ssh_conn_id="kd01_keydriver",command="sh /usr/lib/carter/kd_strategy/script/kdalpha_am_end_task.sh prod ", dag=dag)
 kdalpha_strategy_rank_task >> [kdalpha_am_end_task]
 kdalpha_am_start_task >> [kdalpha_daily_am_task]
 kdalpha_daily_am_task >> [kdalpha_strategy_rank_task]

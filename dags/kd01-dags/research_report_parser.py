c6ded8d8e3dcfe697250c893bb09a2f7bf0f1ffb
wenfeng.yu <ywf690912537>
2021-02-01 15:06:02 +0800

    01 任务

--- dags/kd01-dags/research_report_parser.py
+++ dags/kd01-dags/research_report_parser.py
@@ -1,22 +1,21 @@
 # -*- coding: utf-8 -*-
 from airflow import DAG
-from airflow.contrib.operators.ssh_operator import SSHOperator
+from airflow.operators.bash_operator import BashOperator
 from datetime import datetime
 
 default_args = {
-    'owner': 'keydriver'
+    'owner': 'afroot01'
 }
 
 dag = DAG(
-    'kd01_keydriver_research_report_parser',
+    'kd01_research_report_parser',
     default_args=default_args,
-    description='research_report_parser',
     schedule_interval='0 20 * * *',
-    start_date=datetime(2020, 12, 21, 20, 0)
+    start_date=datetime(2021, 1, 28, 20, 0)
 )
 # ==========================================================tasks======================================================
-parse_research_report = SSHOperator(task_id="parse_research_report", ssh_conn_id="kd01_keydriver",
-                                    command="cd /usr/lib/carter/event-news-scheduler;sh project/extractor/script/parse_research_report.sh dev ", dag=dag)
-research_report_opinion_detection = SSHOperator(task_id="research_report_opinion_detection", ssh_conn_id="kd01_keydriver",command="cd /usr/lib/carter/event-news-scheduler;sh project/extractor/script/opinion_detection.sh dev ", dag=dag)
+parse_research_report = BashOperator(task_id="parse_research_report", 
+                                    bash_command="cd /usr/lib/carter/event-news-scheduler;sh project/extractor/script/parse_research_report.sh dev ", dag=dag)
+research_report_opinion_detection = BashOperator(task_id="research_report_opinion_detection", bash_command="cd /usr/lib/carter/event-news-scheduler;sh project/extractor/script/opinion_detection.sh dev ", dag=dag)
 
 parse_research_report >> research_report_opinion_detection
--- dags/kd01-dags/sync_l2data.py
+++ dags/kd01-dags/sync_l2data.py
@@ -1,19 +1,17 @@
 # -*- coding: utf-8 -*-
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
-from airflow.utils.dates import days_ago
 from datetime import datetime
 
 default_args = {
-    'owner': 'kd01_sync'
+    'owner': 'afroot01'
 }
 
 dag = DAG(
     'sync_l2data',
     default_args=default_args,
-    description='sync-l2data-kd03-kd01',
     schedule_interval='*/10 17-18 * * *',
-    start_date=datetime(2020, 12, 21, 17, 0)
+    start_date=datetime(2021, 1, 28, 17, 0)
 )
 # ==========================================================tasks======================================================
 sync_l2data = BashOperator(task_id="sync_l2data",

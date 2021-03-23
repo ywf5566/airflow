a64495142cff65b319ecc5ea2a5df2ecb0f62f9d
wenfeng.yu <ywf690912537>
2021-03-23 17:00:52 +0800

    kd01 company csv 任务

--- dags/kd01-dags/event_org_company_csv_update.py
+++ dags/kd01-dags/event_org_company_csv_update.py
@@ -11,7 +11,8 @@ dag = DAG(
     default_args=default_args,
     description='event_org_company_csv_update',
     schedule_interval='15 */3 * * *',
-    start_date=datetime(2020, 12, 25, 9, 15)
+    start_date=datetime(2021, 3, 20, 9, 15)
 )
 # ==========================================================tasks======================================================
 event_org_company_csv_update = BashOperator(task_id="event_org_company_csv_update", bash_command="sh /usr/lib/carter/separateDagobahTask/scripts/KeydriverdbProd/updateEventOrgCompany.sh ", dag=dag)
+sync_kd_security_info = BashOperator(task_id="sync_kd_security_info", bash_command="sh /usr/lib/carter/separateDagobahTask/scripts/KeydriverdbProd/syncKdSecurityInfo.sh ")

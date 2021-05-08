d638031cac7949b06004854091e137037a8b38f3
wenfeng.yu <ywf690912537>
2021-05-08 17:23:51 +0800

    2021-05-08 l2 from em sh command

--- dags/kd04-dags/kd05_level2_from_em_to_kd.py
+++ dags/kd04-dags/kd05_level2_from_em_to_kd.py
@@ -12,8 +12,8 @@ dag = DAG('KD05_level2_from_em_to_kd',
           start_date=datetime(2020, 12, 24, 16, 0))
 
 level2_from_em = SSHOperator(task_id="level2_from_em", ssh_conn_id="kd05_keydriver",
-                             command="/usr/lib/quant/factor/factor_repo/tools/l2code/l2source_file/sync.sh ", dag=dag)
+                             command="sh /usr/lib/quant/factor/factor_repo/tools/l2code/l2source_file/sync.sh ", dag=dag)
 level2_to_kd = SSHOperator(task_id="level2_to_kd", ssh_conn_id="kd05_keydriver",
-                           command="/usr/lib/quant/factor/factor_repo/tools/l2code/parser/parser.sh ", dag=dag)
+                           command="sh /usr/lib/quant/factor/factor_repo/tools/l2code/parser/parser.sh ", dag=dag)
 
 level2_from_em >> level2_to_kd

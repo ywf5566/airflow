ba3da7d2a88ed6e3e7c4c014608179e46286b048
wenfeng.yu <ywf690912537>
2021-05-25 16:28:43 +0800

    2021-05-25 stock-3min-source create dag

---
+++ dags/kd05-dags/stock_3minute_source_production.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+
+default_args = {'owner': 'afroot05'}
+dag = DAG('stock_3minute_source_production',
+          default_args=default_args,
+          schedule_interval='30 */2 * * *',
+          catchup=False,
+          start_date=datetime(2021, 5, 25, 12, 30))
+
+stock_3minute_source_production = BashOperator(task_id="stock_3minute_source_production",
+                                               bash_command="sh /usr/lib/carter/stock-3minute-source/stock_3minute_source/scripts/sync_all.sh ",
+                                               dag=dag)

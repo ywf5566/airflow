30787981c8d21c1a3dfedd89a25961ede87a1b5d
wenfeng.yu <ywf690912537>
2021-06-02 17:09:16 +0800

    2021-06-02 05dags add sync star product

---
+++ dags/kd05-dags/STAR_sync_data.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+
+default_args = {'owner': 'afroot05', 'retries': 2, 'retry_delay': timedelta(minutes=1), 'start_date': datetime.strptime('2021-06-01 16:40:00', "%Y-%m-%d %H:%M:%S")}
+
+dag = DAG('STAR_sync_data',
+          default_args=default_args,
+          schedule_interval='15 17 * * *')
+
+sync_kcb_industry = BashOperator(
+    task_id='sync_kcb_industry',
+    bash_command=r'''sh /usr/lib/carter/dbsync/scripts/sync_kcb_industry.sh  ''',
+    dag=dag)
+
+sync_STAR_adjfactor = BashOperator(
+    task_id='sync_STAR_adjfactor', bash_command=r'''sh /usr/lib/carter/dbsync/scripts/sync_kcb_adjfactor.sh  ''',
+    trigger_rule='all_success', dag=dag)
+
